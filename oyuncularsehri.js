60d9a0a47e7c7ceddc2053ae34745bca946a3921
zalim123456 <59143212+zalim123456@users.noreply.github.com>
2020-12-02 14:17:07 +0300

    Add files via upload

---
+++ oyuncularsehri.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { Client, RichEmbed } = require('discord.js');
+const app = express();
+    function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+const http = require('http');
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Oyuncular Şehri | J4J BOT");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000) 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const data = new Map();
+
+client.on('ready', () => {
+        console.log(`${client.user.username} ismi ile giriş yapıldı! ${client.guilds.size} Sunucu, ${client.users.size} Kullanıcı.`);
+});
+
+
+client.on("ready", () => {
+  client.channels.get("783310676536983582").join();
+})
+
+
+client.login("") // user tokeniniz
